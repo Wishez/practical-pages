81063afa2d4f58b934b70bbbeb1081ec3c821f9b
Filipp Zhuravlev <shiningfinger@list.ru>
2017-10-19 16:46:56 +0300

    I've styled and adaptived first three sections.

--- Pilumnus/gulpfile.js
+++ Pilumnus/gulpfile.js
@@ -47,7 +47,7 @@ scssPathes = ['node_modules/susy/sass',
               'node_modules/breakpoint-sass/stylesheets',
              'node_modules/bootstrap-sass/assets/stylesheets',
              'node_modules/font-awesome-sass/assets/stylesheets/',
-             'node_modules/semantic-ui-sass/',];
+             'node_modules/semantic-ui-sass/'];
 
 
 
