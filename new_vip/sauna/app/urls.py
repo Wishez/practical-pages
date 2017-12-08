781ca82148f9e24e59889599a4404646e3c1e5d4
wizhes <shiningfinger@list.ru>
2017-12-08 12:13:12 +0300

    Add base view and home view, afterall include into urls.

--- new_vip/sauna/app/urls.py
+++ new_vip/sauna/app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-
+from .views import *
 urlpatterns = [
-    # url('', name='home'),
+    url('^$', HomePage.as_view(), name='home'),
 ]
--- new_vip/sauna/app/views.py
+++ new_vip/sauna/app/views.py
@@ -1,4 +1,31 @@
 # -*- encoding: utf-8 -*-
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+class BaseView(TemplateView):
+    def __init__(self):
+        self.title = 'Home'
+        self.meta = 'Meta'
+        self.setting = None
+        self.info = None
+
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseView, self).get_context_data(**kwargs)
+
+        context['meta'] = self.meta
+        context['title'] = self.title
+        context['setting'] = self.setting
+        context['info'] = self.info
+
+        return context
+
+class HomePage(BaseView):
+    template_name = 'index.html'
+
+    # def get(self, request):
+    #     return render(
+    #         request,
+    #         self.template_name,
+    #         super().get_context_data(),
+    #     )
