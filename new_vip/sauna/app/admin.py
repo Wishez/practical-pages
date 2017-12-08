1e7a10461a1908c50f4514bf274dd1350a75519c
wizhes <shiningfinger@list.ru>
2017-12-08 21:11:37 +0300

    Add filtering models in admin by created date.

--- new_vip/sauna/app/admin.py
+++ new_vip/sauna/app/admin.py
@@ -9,14 +9,16 @@ from .models import *
 class ScheduleAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_display = ('title', 'condition_1', 'condition_2', 'condition_3', 'note',)
-    filter_fields = ('title', 'condition_1', 'condition_2', 'condition_3',)
+    filter_fields = ('title', 'condition_1', 'condition_2', 'condition_3', 'created',)
     search_fields = ('title', 'condition_1', 'condition_2', 'condition_3', 'note',)
+    date_hierarchy = 'created'
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_per_page = 10
-    filter_fields = ('name',)
+    filter_fields = ('name', 'created',)
     search_fields = ('name',)
+    date_hierarchy = 'created'
 @admin.register(Settings)
 class SettingsAdmin(SingleModelAdmin):
     fieldsets = (
