c5219ee3aca546c9dee9793208a439098c69d1c8
wizhes <shiningfinger@list.ru>
2017-12-10 00:14:35 +0300

    Change views' names.

--- new_vip/sauna/pages/urls.py
+++ new_vip/sauna/pages/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url('^services/$', ServicesView.as_view(), name='services'),
     url('^prices/$', PricesView.as_view(), name='prices'),
     url('^contacts/$', ContactsView.as_view(), name='contacts'),
-    url('^sauna/(?P<slug>[a-z]+)/$', SaunaView.as_view(), name='sauna'),
+    url('^sauna/(?P<slug>[a-z_]+)/$', SaunaView.as_view(), name='sauna'),
 ]
