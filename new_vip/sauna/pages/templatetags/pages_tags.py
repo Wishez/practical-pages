f6737d7e7d68ae94315bd324eb0dc0162f087e88
wizhes <shiningfinger@list.ru>
2017-12-10 00:01:22 +0300

    Create the screwdriver for  templates.

--- new_vip/sauna/pages/templatetags/pages_tags.py
+++ new_vip/sauna/pages/templatetags/pages_tags.py
@@ -10,8 +10,7 @@ def get_array(model):
     return model.all()
 def get_photo(id):
     return AlbumImage.objects.filter(album=id)
-def ugly_phone(phone):
-    return ''
+
 register.filter('get_responsive_height', get_responsive_height)
 register.filter('get_array', get_array)
 register.filter('get_photo', get_photo)
